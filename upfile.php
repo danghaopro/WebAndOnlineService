23112c1bca8326a98a6a9cc19eb290e3988f03a2
Duc Minh Dao <daoducminh1997@gmail.com>
2018-03-11 17:34:19 +0700

    Lab 4 Ex 1
    các ông giúp tôi sửa phần code đoạn upload file nằm trong file 'upfile.php' nhé

---
+++ upfile.php
@@ -0,0 +1,29 @@
+<?php
+if (isset($_POST['submit'])) {
+	$number = $_POST['number'];
+    echo "You have chosen to upload ".$number." files";
+    echo "<form method=post>";
+	echo "<input type=\"file\" name=\"myFile\" multiple><br>";
+	echo "<input type=\"submit\" name=\"upload\" value=\"Upload\"></form>";
+
+    if (isset($_FILES['myFile'])) 
+    {
+    $myFile = $_FILES['myFile'];
+    $fileCount = count($myFile["name"]);
+
+    if($fileCount>$number)
+    {
+        echo "<p>You have uploaded more files than you submitted</p>";
+    }
+    else
+    {
+        for ($i = 0; $i < $fileCount; $i++) {                   //http://php.net/manual/en/function.move-uploaded-file.php
+            $filename = $_FILES['upload']['name'][$i];          //tên file khi upload lên thư mục (destination)
+            $file_tmp = $_FILES['upload']['tmp_name'][$i];      //tên file
+            move_uploaded_file($file_tmp,"upload/".$filename);  //chuyển file vào thư mục "upload/" với tên là filename
+        }
+    }
+}
+}
+
+?>
