661d7cb2e1bf5335b3acd231ebc4d2dbe386d6c2
Phạm Duy Anh <37221121+anhpd1004@users.noreply.github.com>
2018-03-21 19:49:10 +0700

    Add files via upload

--- Lab_7/Lab7_5_Validate.php
+++ Lab_7/Lab7_5_Validate.php
@@ -17,63 +17,55 @@
 	</head>
 	<body>
 		<h2>The result for validate Email address, URL address and Phone number.</h2>
+		
+		<?php  
 
-		<?php
+			class MyValidate {
 
-            class MyValidate
-            {
-                public function __construct()
-                {
-                }
+				public function __construct() {
 
-                public function IsValidEmail($email)
-                {
-                    return filter_var($email, FILTER_VALIDATE_EMAIL) && preg_match('/@.+\./', $email);
-                }
-                public function IsValidURL($url)
-                {
-                    return filter_var($url, FILTER_VALIDATE_URL) && preg_match('/\/{2,}\.+?*/', $url);
-                }
-                public function IsValidPhoneNumber($phone)
-                {
-                    $pattern = '/[0-9]{6,14}/';
-                    return preg_match($pattern, $phone);
-                }
-            }
+				}
+
+				public function IsValidEmail($email) {
+					return filter_var($email, FILTER_VALIDATE_EMAIL) && preg_match('/@.+\./', $email);
+				}
+				public function IsValidURL($url) {
+					return filter_var($url, FILTER_VALIDATE_URL);
+				}
+				public function IsValidPhoneNumber($phone) {
+					$pattern = '/[0-9]{6,14}/';
+					return preg_match($pattern, $phone);
+				}
+			}
 
 
-            $email = "";
-            $url = "";
-            $phone = "";
-            if (isset($_POST['submit'])) {
-                if (isset($_POST['email'])) {
-                    $email = trim($_POST['email']);
-                }
-                if (isset($_POST['phone'])) {
-                    $phone = trim($_POST['phone']);
-                }
-                if (isset($_POST['url'])) {
-                    $url = trim($_POST['url']);
-                }
-            }
-            $validate = new MyValidate();
-            if ($validate->IsValidEmail($email)) {
-                echo "<b>Valid Email address = $email</b><br>";
-            } else {
-                echo "<i>Invalid Email address = $email</i><br>";
-            }
-            if ($validate->IsValidURL($url)) {
-                echo "<b>Valid URL address = $url</b><br>";
-            } else {
-                echo "<i>Invalid URL address = $url</i><br>";
-            }
-            if ($validate->IsValidPhoneNumber($phone)) {
-                echo "<b>Valid phone number = $phone</b><br>";
-            } else {
-                echo "<i>Invalid phone number = $phone</i><br>";
-            }
+			$email = "";
+			$url = "";
+			$phone = "";
+			if(isset($_POST['submit'])) {
+				if(isset($_POST['email']))
+					$email = trim($_POST['email']);
+				if(isset($_POST['phone']))
+					$phone = trim($_POST['phone']);
+				if(isset($_POST['url']))
+					$url = trim($_POST['url']);
+			}
+			$validate = new MyValidate();
+			if($validate->IsValidEmail($email)) {
+				echo "<b>Valid Email address = $email</b><br>";
+			}
+			else 
+				echo "<i>Invalid Email address = $email</i><br>";
+			if($validate->IsValidURL($url)) 
+				echo "<b>Valid URL address = $url</b><br>";
+			else 
+				echo "<i>Invalid URL address = $url</i><br>";
+			if($validate->IsValidPhoneNumber($phone)) 
+				echo "<b>Valid phone number = $phone</b><br>";
+			else 
+				echo "<i>Invalid phone number = $phone</i><br>";
 
-        ?>
+		?>
 		<p>
 			<a href="Lab7_Ex4.php" title="Quay lại trang trước."><button type="button">Return</button></a>
 		</p>
