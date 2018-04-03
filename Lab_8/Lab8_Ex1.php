f3a3b503e8a69923df7cffa33329a67ac1d2dd2f
Phạm Duy Anh <37221121+anhpd1004@users.noreply.github.com>
2018-04-03 22:07:41 +0700

    Add files via upload

---
+++ Lab_8/Lab8_Ex1.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exercise 8.8</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="Lab8_Ex1.css">
+</head>
+<body>
+	<!-- header page -->
+	<div class="header">
+		<div>
+			<ul class="header_menu">
+				<li><a href="#" title="">Home</a></li>
+				<li><a href="#" title="">Game</a></li>
+				<li><a href="#" title="">Music</a></li>
+				<li><a href="#" title="">Entertainment</a></li>
+				<li><a href="#" title="">About</a></li>
+			</ul>
+		</div>
+	</div>
+	<!-- body page -->
+	<div class="body">
+		<!-- main content -->
+		<div class="main_content">
+			<h1>Main content</h1>
+			Main contentMain contentMain contentMain contentMain contentMain contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar content
+		</div>
+		<!-- sidebar -->
+		<div class="sidebar">
+			Sidebar content<br>
+			Sidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar contentSidebar content
+			<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+			<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+			<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+			<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+		</div>
+	</div>
+	<!-- footer page -->
+	<div class="footer">
+		Footer content
+	</div>
+</body>
+</html>
