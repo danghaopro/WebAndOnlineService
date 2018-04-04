2e5d2ed0d1ef9887dcbb116273a9ac1476169ff8
Phạm Duy Anh <37221121+anhpd1004@users.noreply.github.com>
2018-04-04 21:06:08 +0700

    Add files via upload

---
+++ Lab_8/Lab8_Ex1_1.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tin nhanh</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="Lab8_Ex1.css">
+</head>
+<body>
+	<!-- header page -->
+	<div class="header">
+		<div>
+			<ul class="header_menu">
+				<li><a href="#" title="">Home</a></li>
+				<li><a href="#" title="">Thời sự</a></li>
+				<li><a href="#" title="">Giải trí</a></li>
+				<li><a href="#" title="">Người đẹp</a></li>
+				<li><a href="#" title="">Pháp luật</a></li>
+				<li><a href="#" title="">Thể thao</a></li>
+				<li><a href="#" title="">Quân sự</a></li>
+				<li><a href="#" title="">Giáo dục</a></li>
+				<li><a href="#" title="">Kinh tế</a></li>
+				<li><a href="#" title="">Chính trị</a></li>
+				<li><a href="#" title="">Tài chính</a></li>
+				<li><a href="#" title="">Tâm sự</a></li>
+			</ul>
+		</div>
+	</div>
+	<!-- body page -->
+	<div class="body">
+		<!-- main content -->
+		<div class="main_content">
+			<h1>Xét xử kỳ án đổi vé số lấy... sex</h1>
+			<h3>Sau 2 ngày xét xử (ngày 3 và 4 – 4), TAND huyện An Phú (tỉnh An Giang) đưa ra mức án đối với bị cáo Bùi Văn Sơn (Út Lượm, 51 tuổi, ĐKTT: ấp An Hưng, thị trấn An Phú, huyện An Phú) về tội giao cấu với trẻ em. Đây là vụ án thu hút sự quan tâm đặc biệt của dư luận. </h3>
+			<p>Theo nội dung cáo trạng, vào cuối tháng 8-2011, H.T.T.L. (22 tuổi) đang bán vé số kiến thiết tại quán cà phê trên địa bàn thị trấn An Phú gặp Bùi Văn Sơn mua khoảng 30 – 40 tờ vé số kiến thiết loại 10.000 đồng.</p>
+			<p>Sơn hẹn L. chiều đến nhà tại ấp An Hưng, sẽ trả tiền, do quen biết L. đồng ý. Chiều cùng ngày, thấy L. đến nhà Sơn để nhận tiền.</p>
+			<p>Tại đây, Sơn kêu L. cùng vào phòng ngủ tầng trệt để lấy tiền. Khi L. vào phòng Sơn đóng cửa và kêu L. cho giao cấu muốn gì cũng được. L. đồng ý và giữa 2 người giao cấu với nhau. Sau khoảng 10 phút, Sơn đưa L. 1 triệu đồng gồm cả tiền vé số và tiêu xài.</p>
+			<p>Sao lần giao cấu trên, lợi dụng việc mua vé số, Sơn hẹn L. đến nhà, giao cấu thêm 2 lần, cách thức như lần đầu. Ngoài ra Sơn còn giao cấu với L. 6 lần tại nhà nghỉ trên địa bàn. Sau mỗi lần gặp giao cấp Sơn trả tiền vé số và cho tiền L. từ 100.000 đồng đến 1 triệu đồng</p>
+			<p>Đến tháng 2-2012, L. phát hiện mình có thai và tìm đến gặp Sơn báo tin, xin tiền phá thai, Sơn không cho. Lúc này L. về nhà nới với gia đình và nhiều người biết là Sơn giao cấu với L.. Gia đình bị hại tiến hành gửi đơn tố giác đến Cơ quan chức năng, yêu cầu xử lí Bùi Văn Sơn. Hơn 2 năm điều tra, vụ việc vẫn không được làm sáng tỏa.</p>
+			<p>Ông Sơn vẫn không thừa nhận quan hệ tình dục với L., Cơ quan điều tra 3 lần trưng cầu giám định ADN đứa bé và ông Sơn, nhưng có kết quả không cùng huyết thống. Hai phía tiến hành thỏa thuận với nhau nhiều lần nhưng không thành… Gia đình L. vẫn không chấp nhận, gửi đơn khiếu nại đến các cơ quan chức năng.</p>
+			<p>Căn cứ bản giám định ngày 22-5-2012 của Phân viện Khoa học hình sự - Bộ Công an xác định: Tại thời điểm giám định L. có độ tuổi từ 14 năm 5 tháng đến 14 năm 8 tháng. Tại Cơ quan điều tra, bị can Sơn khai: Do Sơn thường uống rượu, bia vào nhà nghỉ thuê phòng nằm nghỉ đến khi khỏe mới về nhà, tránh cãi nhau với vợ. Khi mở cửa phòng Sơn mở cửa cho thoáng lên giường nằm L. vào bán vé số, Sơn mua trả tiền xong. L. đi ra, không giao cấu với L. Còn về việc, gia đình Sơn giao cho gia đình L. 300 triệu đồng thì Sơn không biết.</p>
+			<p>Đại diện VKSND cho rằng, từ cuối tháng 8-2011 đến ngày 12-2-2012, Bùi Văn Sơn đã có hành vi nhiều lần (8 lần) giao cấu với L. tại nhà của Sơn và nhà nghỉ. Vào thời điểm giao cấu, L. trong độ tuổi 13 – dưới 16 tuổi. Vì vậy, bị cáo Sơn phải chịu trách nhiệm hình sư về hành vi phạm tội của mình.</p>
+			<p>Kết thúc phiên tòa, HĐXX cho rằng bị cáo là người trưởng thành, đã có vợ và chỉ vì ham muốn đã đánh mất danh dự của mình. Hành vi này gây nguy hiểm cho xã hội … Do vậy, sau khi áp dụng nhiều tình tiết giảm nhẹ, HĐXX tuyên phạt bị cáo Sơn 3 năm tù giam.</p>
+
+		</div>
+		<!-- sidebar -->
+		<div class="sidebar">
+			<p class="title_sidebar_menu">Sidebar menu</p>
+			<ul class="sidebar_menu">
+				<li><a href="#" title="">Tin gần đây</a></li>
+				<li><a href="#" title="">Tin hot</a></li>
+				<li><a href="#" title="">Tin liên quan</a></li>
+				<li><a href="#" title="">Tin giật gân</a></li>
+				<li><a href="#" title="">Tin không liên quan</a></li>
+			</ul>
+			<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+			<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+		</div>
+	</div>
+	<!-- footer page -->
+	<div class="footer">
+		Nhóm 28 &copy; Công nghệ web và dịch vụ trực tuyến
+	</div>
+</body>
+</html>
